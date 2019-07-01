ba5d8c35bf7fdeef56b90b4b98f7a8d0a1ce416d
Vishnu Srinivas <vishnu.8913@gmail.com>
2019-07-01 09:05:38 -0700

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+import org.jfugue.player.Player;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+	Player player = new Player();
+	player.play("T200 C4h C4h G4h G4h A4h A4h G4w F4h F4h E4h E4h D4q D4q D4q D4q C4w");
+	}
+	
+
+}
